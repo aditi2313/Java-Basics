09c5a58585548db168114d3d0830e0f403f43081
aditi2313 <guptaaditi2313@gmail.com>
2024-12-23 14:37:50 +0800

    ArrayList Notes

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Dog d = new Dog("Spot", 1);
@@ -10,7 +12,16 @@ public class App {
         Person q = new Person("Bob", 20, Demographic.YOUNG);
         System.out.println(q);
         System.out.println(q.getPet());
+
         int[] numbers = { 1, 2, 3, 4, 5 };
         System.out.println(numbers.length);
+
+        ArrayList<Integer> moreNumbers = new ArrayList<>();
+        moreNumbers.add(10);
+        moreNumbers.add(20);
+        moreNumbers.add(30);
+        moreNumbers.remove(1); // Removes the element at index 1 (20)
+        System.out.println(moreNumbers);
+        System.out.println(moreNumbers.size());
     }
 }
