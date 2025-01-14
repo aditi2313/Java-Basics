2a56af12e86a344b5663d224323c2e9806ba3150
aditi2313 <guptaaditi2313@gmail.com>
2025-01-14 12:57:52 +0800

    QuickSort Prep

---
+++ src/QuickSort1.java
@@ -0,0 +1,36 @@
+public class QuickSort1 {
+    public static void main(String[] args) {
+        int[] numbers = { 0, 3, 8, 1, 2, 7 };
+        quicksort(numbers, 0, numbers.length - 1);
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+
+    public static void quicksort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quicksort(arr, low, pivotIndex - 1);
+            quicksort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
